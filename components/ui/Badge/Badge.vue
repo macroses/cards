2c7da5fc190ddb5f074716fa43731a24e9440140
konstantinbaskov <bashkovdev@icloud.com>
2024-10-21 23:21:55 +0400

    styling

--- components/ui/Badge/Badge.vue
+++ components/ui/Badge/Badge.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
+const props = defineProps<{
+  color?: string
+}>()
 
+const styleObject = reactive({
+  color: `rgb(${props.color})`,
+  backgroundColor: `rgba(${props.color}, 0.1)`,
+  border: `1px solid rgba(${props.color}, 0.5)`,
+})
 </script>
 
 <template>
-  <div class="badge">
+  <div
+    class="badge"
+    :style="styleObject"
+  >
     <slot />
   </div>
 </template>
