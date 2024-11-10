42281c61f017a5402db803728d12204a730984fe
konstantinbaskov <bashkovdev@icloud.com>
2024-11-10 16:04:09 +0400

    full redesign

--- components/ui/TheLoader/TheLoader.vue
+++ components/ui/TheLoader/TheLoader.vue
@@ -38,11 +38,8 @@ onMounted(() => {
 
 <style scoped>
 .loader {
-  width: 100%;
-  height: 100%;
   position: absolute;
-  top: 0;
-  left: 0;
+  inset: 0;
   display: flex;
   align-items: center;
   justify-content: center;
