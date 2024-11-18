7b721bd887fe039b21e7508fd875fa2fa84a1b87
konstantinbaskov <bashkovdev@icloud.com>
2024-11-18 13:01:23 +0400

    rework

--- components/Workout/WorkoutExercises/WorkoutExercises.vue
+++ components/Workout/WorkoutExercises/WorkoutExercises.vue
@@ -7,6 +7,8 @@ defineProps<{
 
 const emit = defineEmits<{
   (event: 'removeExercise', id: number): void
+  (event: 'addSet', id: number): void
+  (event: 'removeSet', id: number): void
 }>()
 </script>
 
@@ -57,7 +59,7 @@ const emit = defineEmits<{
           </TheButton>
         </div>
 
-        <!--        <div class="exercise-form__wr">
+        <div class="">
           <form
             v-auto-animate
             class="exercise-form"
@@ -65,80 +67,28 @@ const emit = defineEmits<{
           >
             <div class="exercise-form__main">
               <TheInput
-                v-model.number="getExerciseData(exercise.id).currentWeight"
                 placeholder="Вес"
                 type="number"
                 inputmode="numeric"
                 no-clear
-                :validate-rules="workoutSetRules"
-                @validation="isWorkoutSetValid = $event"
               />
               <TheInput
-                v-model.number="getExerciseData(exercise.id).currentRepeats"
                 placeholder="Повторения"
                 type="number"
                 inputmode="numeric"
                 no-clear
-                :validate-rules="workoutSetRules"
-                @validation="isWorkoutSetValid = $event"
               />
             </div>
             <div class="exercise-form__submit">
-              <WorkoutDifficulty
-                v-model="getExerciseData(exercise.id).currentDifficulty"
-              />
+              <WorkoutDifficulty />
               <TheButton
                 type="submit"
-                :disabled="!isWorkoutSetValid"
               >
                 Append
               </TheButton>
             </div>
-
-            <ul
-              v-if="workoutExercisesLength(exercise.id)"
-              v-auto-animate
-              class="workout-form__sets"
-            >
-              <li class="workout-form__sets-header">
-                <div />
-                <div class="workout-form__sets-header&#45;&#45;weight">
-                  Вес
-                </div>
-                <div class="workout-form__sets-header&#45;&#45;repeats">
-                  Repeats
-                </div>
-                <div class="workout-form__sets-header&#45;&#45;delete" />
-              </li>
-              <li
-                v-for="set in workoutExercises.find(e => e.exerciseId === exercise.id)?.sets"
-                :key="set.id"
-                class="workout-form__sets-item"
-              >
-                <div
-                  class="workout-form__sets&#45;&#45;difficulty"
-                  :data-difficulty="set.difficulty"
-                />
-                <div class="workout-form__sets&#45;&#45;weight">
-                  {{ set.weight }}
-                </div>
-                <div class="workout-form__sets&#45;&#45;repeats">
-                  {{ set.repeats }}
-                </div>
-                <TheButton
-                  variant="transparent"
-                  icon-only
-                  @click="emit('removeSet', exercise.id, set.id)"
-                >
-                  <TheIcon
-                    icon-name="xmark"
-                    width="16px"
-                  />
-                </TheButton>
-              </li>
-            </ul>
           </form>
-        </div> -->
+        </div>
       </li>
     </ul>
     <p
