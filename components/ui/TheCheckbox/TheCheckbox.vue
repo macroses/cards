85c2f48e17e7dcc06df909821665d744679ece72
konstantinbaskov <bashkovdev@icloud.com>
2024-11-27 21:26:21 +0400

    credentials

---
+++ components/ui/TheCheckbox/TheCheckbox.vue
@@ -0,0 +1,50 @@
+<script setup lang="ts">
+defineProps({
+  value: {
+    type: [String, Number],
+    default: '',
+  },
+  label: {
+    type: String,
+    default: '',
+  },
+})
+
+const uniqueId = useId()
+
+const checked = defineModel('checked')
+</script>
+
+<template>
+  <div class="checkbox-parent">
+    <div class="checkbox-content">
+      <div class="checkbox">
+        <input
+          :id="uniqueId"
+          v-model="checked"
+          class="checkbox-component"
+          type="checkbox"
+        >
+        <div
+          class="checkbox-effect"
+          :class="{ active: checked }"
+        >
+          <div class="checkbox-icon">
+            <div class="line1" />
+            <div class="line2" />
+          </div>
+        </div>
+      </div>
+
+      <label
+        v-if="label"
+        :for="uniqueId"
+        class="checkbox-label"
+      >
+        {{ label }}
+      </label>
+    </div>
+  </div>
+</template>
+
+<style src="./style.css"></style>
