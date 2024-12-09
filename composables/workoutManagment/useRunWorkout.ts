88ed130355434502ebd722e9f191fe972985402e
konstantinbaskov <bashkovdev@icloud.com>
2024-12-09 17:05:01 +0400

    inputs

--- composables/workoutManagment/useRunWorkout.ts
+++ composables/workoutManagment/useRunWorkout.ts
@@ -3,6 +3,7 @@ import type { CreateWorkoutResponse } from '~/ts/interfaces'
 
 interface RunWorkoutReturn {
   runWorkout: ComputedRef<CreateWorkoutResponse | null | undefined>
+  originalWorkout: Ref<CreateWorkoutResponse | null>
   initRunMode: () => Promise<void>
 }
 
@@ -10,6 +11,7 @@ export function useRunWorkout(): RunWorkoutReturn {
   const route = useRoute()
   const workoutsList = useState<CreateWorkoutResponse[] | []>(GLOBAL_WORKOUTS)
   const workoutRunId = ref<string | null>(null)
+  const originalWorkout = ref<CreateWorkoutResponse | null>(null)
 
   const runWorkout = computed(() => {
     if (!workoutRunId.value)
@@ -18,12 +20,20 @@ export function useRunWorkout(): RunWorkoutReturn {
     return workoutsList.value?.find((workout: CreateWorkoutResponse) => workout.id === workoutRunId.value)
   })
 
+  watch(runWorkout, (workout) => {
+    if (workout && !originalWorkout.value) {
+      // Создаем копию только при первой инициализации
+      originalWorkout.value = JSON.parse(JSON.stringify(workout))
+    }
+  }, { immediate: true })
+
   async function initRunMode() {
     workoutRunId.value = route.params.id as string
   }
 
   return {
     runWorkout,
+    originalWorkout,
     initRunMode,
   }
 }
