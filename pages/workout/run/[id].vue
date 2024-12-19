b1234f83abfb3a214fdff5bf3035b24e2c06bc73
konstantinbaskov <bashkovdev@icloud.com>
2024-12-19 11:25:24 +0400

    styling run workout

--- pages/workout/run/[id].vue
+++ pages/workout/run/[id].vue
@@ -6,11 +6,19 @@ interface EditingState {
   field: 'weight' | 'repeats' | null
 }
 
+interface SetTime {
+  minutes: number
+  seconds: number
+}
+
 const { runWorkout, initRunMode, originalWorkout } = useRunWorkout()
 const { endWorkout } = useFinishWorkout()
 const { getData } = useRunWorkoutChart()
+const { activeWorkout } = useWorkoutTimer()
 const activeExercises = ref<Set<number>>(new Set())
 const option = shallowRef(getData(originalWorkout.value, runWorkout.value, activeExercises.value))
+const setTimes = ref<Record<string, SetTime>>({})
+const lastSetTime = ref<number | null>(null)
 
 async function handleFinishWorkout() {
   if (runWorkout.value) {
@@ -82,6 +90,29 @@ function handleInputChange(event: Event, set: any, field: 'weight' | 'repeats') 
   }
 }
 
+function formatTime(time: SetTime): string {
+  return `${time.minutes}:${time.seconds.toString().padStart(2, '0')}`
+}
+
+function handleSetTime(setId: string) {
+  const currentTime = Date.now()
+
+  if (lastSetTime.value === null && activeWorkout.value?.startedAt) {
+    const timeDiff = currentTime - new Date(activeWorkout.value.startedAt).getTime()
+    const minutes = Math.floor(timeDiff / 60000)
+    const seconds = Math.floor((timeDiff % 60000) / 1000)
+    setTimes.value[setId] = { minutes, seconds }
+  }
+  else if (lastSetTime.value) {
+    const timeDiff = currentTime - lastSetTime.value
+    const minutes = Math.floor(timeDiff / 60000)
+    const seconds = Math.floor((timeDiff % 60000) / 1000)
+    setTimes.value[setId] = { minutes, seconds }
+  }
+
+  lastSetTime.value = currentTime
+}
+
 watchEffect(() => {
   option.value = getData(originalWorkout.value, runWorkout.value, activeExercises.value)
 })
@@ -96,119 +127,126 @@ onMounted(async () => {
 </script>
 
 <template>
-  <h1
-    v-if="runWorkout"
-    class="run__title"
-  >
-    {{ runWorkout.title }}
-  </h1>
-  <div
-    v-if="runWorkout"
-    class="run"
-  >
-    <div class="run__current">
-      <ul class="run__exercises-list">
-        <li
-          v-for="exercise in runWorkout.exercises"
-          :key="exercise.exerciseId"
-          class="run__exercise-item"
-        >
-          <div
-            class="run__exercise"
-            :class="{ active: activeExercises.has(exercise.exerciseId) }"
-            @click="toggleExercise(exercise.exerciseId)"
+  <div class="run-template">
+    <h1
+      v-if="runWorkout"
+      class="run__title"
+    >
+      {{ runWorkout.title }}
+    </h1>
+    <div
+      v-if="runWorkout"
+      class="run"
+    >
+      <div class="run__current">
+        <ul class="run__exercises-list">
+          <li
+            v-for="exercise in runWorkout.exercises"
+            :key="exercise.exerciseId"
+            class="run__exercise-item"
           >
-            <TheIcon
-              icon-name="angle-down"
-              width="14px"
-            />
-            {{ exercise.exerciseName }}
-          </div>
-          <div
-            class="run__sets-container"
-            :class="{ active: activeExercises.has(exercise.exerciseId) }"
-          >
-            <ul
-              v-if="exerciseSets[exercise.exerciseId]?.length"
-              class="run__sets"
+            <div
+              class="run__exercise"
+              :class="{ active: activeExercises.has(exercise.exerciseId) }"
+              @click="toggleExercise(exercise.exerciseId)"
             >
-              <li
-                v-for="set in exerciseSets[exercise.exerciseId]"
-                :key="set.id"
-                class="run__set-item"
+              <TheIcon
+                icon-name="angle-down"
+                width="14px"
+              />
+              {{ exercise.exerciseName }}
+            </div>
+            <div
+              class="run__sets-container"
+              :class="{ active: activeExercises.has(exercise.exerciseId) }"
+            >
+              <ul
+                v-if="exerciseSets[exercise.exerciseId]?.length"
+                class="run__sets"
               >
-                <div class="run__set-difficulty">
-                  {{ set.difficulty }}
-                </div>
-                <div
-                  class="run__set-weight"
-                  @click="handleEdit(set.id, 'weight')"
+                <li
+                  v-for="set in exerciseSets[exercise.exerciseId]"
+                  :key="set.id"
+                  class="run__set-item"
                 >
-                  <TheInput
-                    v-if="editingState.setId === set.id && editingState.field === 'weight'"
-                    :ref="setInputRef(set.id)"
-                    v-model="set.weight"
-                    placeholder="Вес"
-                    type="number"
-                    inputmode="numeric"
-                    @keyup.enter="handleInputSubmit"
-                    @blur="handleInputSubmit"
-                    @input="handleInputChange($event, set, 'weight')"
-                  />
-                  <div
-                    v-else
-                    class="run__set-weight--value"
-                  >
-                    {{ set.weight }}
+                  <div class="run__set-difficulty">
+                    {{ set.difficulty }}
                   </div>
-                </div>
-                <div
-                  class="run__set-repeats"
-                  @click="handleEdit(set.id, 'repeats')"
-                >
-                  <TheInput
-                    v-if="editingState.setId === set.id && editingState.field === 'repeats'"
-                    :ref="setInputRef(set.id)"
-                    v-model="set.repeats"
-                    placeholder="Повторения"
-                    type="number"
-                    inputmode="numeric"
-                    @keyup.enter="handleInputSubmit"
-                    @blur="handleInputSubmit"
-                    @input="handleInputChange($event, set, 'repeats')"
-                  />
                   <div
-                    v-else
-                    class="run__set-repeats--value"
+                    class="run__set-weight"
+                    @click="handleEdit(set.id, 'weight')"
                   >
-                    {{ set.repeats }}
+                    <TheInput
+                      v-if="editingState.setId === set.id && editingState.field === 'weight'"
+                      :ref="setInputRef(set.id)"
+                      v-model="set.weight"
+                      placeholder="Вес"
+                      type="number"
+                      inputmode="numeric"
+                      @keyup.enter="handleInputSubmit"
+                      @blur="handleInputSubmit"
+                      @input="handleInputChange($event, set, 'weight')"
+                    />
+                    <div
+                      v-else
+                      class="run__set-weight--value"
+                    >
+                      {{ set.weight }}
+                    </div>
                   </div>
-                </div>
-                <div class="time" />
-                <TheButton
-                  variant="ghost"
-                  icon-only
-                >
-                  <TheIcon
-                    icon-name="circle-check"
-                    width="18px"
-                  />
-                </TheButton>
-              </li>
-            </ul>
-          </div>
-        </li>
-      </ul>
-      <button @click="handleFinishWorkout">
-        finish
-      </button>
-    </div>
+                  <div
+                    class="run__set-repeats"
+                    @click="handleEdit(set.id, 'repeats')"
+                  >
+                    <TheInput
+                      v-if="editingState.setId === set.id && editingState.field === 'repeats'"
+                      :ref="setInputRef(set.id)"
+                      v-model="set.repeats"
+                      placeholder="Повторения"
+                      type="number"
+                      inputmode="numeric"
+                      @keyup.enter="handleInputSubmit"
+                      @blur="handleInputSubmit"
+                      @input="handleInputChange($event, set, 'repeats')"
+                    />
+                    <div
+                      v-else
+                      class="run__set-repeats--value"
+                    >
+                      {{ set.repeats }}
+                    </div>
+                  </div>
+                  <div class="time">
+                    {{ setTimes[set.id] ? formatTime(setTimes[set.id]) : '--:--' }}
+                  </div>
+                  <TheButton
+                    v-if="!setTimes[set.id]"
+                    variant="ghost"
+                    icon-only
+                    class="run__set-time"
+                    @click="handleSetTime(set.id)"
+                  >
+                    <TheIcon
+                      icon-name="circle-check"
+                      width="18px"
+                    />
+                  </TheButton>
+                </li>
+              </ul>
+            </div>
+          </li>
+        </ul>
+        <button @click="handleFinishWorkout">
+          finish
+        </button>
+      </div>
 
-    <div class="run__initial">
-      <VChart :option="option" />
+      <div class="run__initial">
+        <VChart :option="option" />
+      </div>
     </div>
-  </div>
-  <div v-else>
-    Тренировка не найдена
+    <div v-else>
+      Тренировка не найдена
+    </div>
   </div>
 </template>
