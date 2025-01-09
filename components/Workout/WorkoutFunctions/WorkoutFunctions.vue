fb49ee927c80f3ca2a61efd6d6ac78c8b66d9289
konstantinbaskov <bashkovdev@icloud.com>
2025-01-09 21:23:26 +0400

    styling run workout

--- components/Workout/WorkoutFunctions/WorkoutFunctions.vue
+++ components/Workout/WorkoutFunctions/WorkoutFunctions.vue
@@ -1,21 +1,13 @@
 <script setup lang="ts">
-import RunWorkoutModal from '~/components/RunWorkoutModal/RunWorkoutModal.vue'
 import TheModal from '~/components/ui/TheModal/TheModal.vue'
-
-interface WorkoutFunctionsProps {
-  workoutId: string
-  workoutTitle: string
-  isWorkoutCompleted?: boolean
-  isCopyMode?: boolean
-  isWorkoutActive?: boolean
-}
+import type { WorkoutFunctionsProps } from '~/ts/componentProps'
 
 const props = withDefaults(defineProps<WorkoutFunctionsProps>(), {
   isCopyMode: false,
   isWorkoutActive: false,
 })
 
-defineEmits<{
+const emit = defineEmits<{
   (event: 'deleteWorkout'): void
   (event: 'copyWorkout'): void
   (event: 'updateWorkout'): void
@@ -23,8 +15,8 @@ defineEmits<{
 
 const { t } = useI18n()
 const localePath = useLocalePath()
-const runWorkoutConfirm = ref<InstanceType<typeof RunWorkoutModal> | null>(null)
-const readWorkoutResults = ref<InstanceType<typeof TheModal> | null>(null)
+const runWorkoutConfirm = ref<typeof TheModal | null>(null)
+const readWorkoutResults = ref<typeof TheModal | null>(null)
 const { startWorkout } = useStartWorkout()
 const { activeWorkout } = useWorkoutTimer()
 
@@ -38,16 +30,20 @@ const showStartButton = computed(() => {
 
 function openRunWorkoutConfirm() {
   if (props.isWorkoutActive) {
+    // Если это активная тренировка, сразу переходим на страницу
     navigateTo(localePath(`/workout/run/${props.workoutId}`))
     return
   }
 
   runWorkoutConfirm.value?.openModal()
 }
-
 async function handleStartWorkout() {
   await startWorkout(props.workoutId)
   navigateTo(localePath(`/workout/run/${props.workoutId}`))
+}
+
+function closeRunWorkoutConfirm() {
+  runWorkoutConfirm.value?.closeModal()
 }
 
 function showResultModal() {
@@ -68,6 +64,53 @@ function showResultModal() {
       {{ workoutTitle }}
     </div>
 
+    <ul
+      class="date-menu__functions"
+    >
+      <li class="date-menu__functions-item">
+        <TheButton
+          v-tooltip="t('main_navigation.copy_workout')"
+          variant="ghost"
+          icon-only
+          @click.stop="emit('copyWorkout')"
+        >
+          <TheIcon
+            icon-name="copy"
+            width="18px"
+          />
+        </TheButton>
+      </li>
+      <li
+        v-if="!isWorkoutCompleted && !isWorkoutActive"
+        class="date-menu__functions-item"
+      >
+        <TheButton
+          v-tooltip="t('main_navigation.edit_workout')"
+          variant="ghost"
+          icon-only
+          @click="emit('updateWorkout')"
+        >
+          <TheIcon
+            icon-name="pen-to-square"
+            width="18px"
+          />
+        </TheButton>
+      </li>
+      <li class="date-menu__functions-item">
+        <TheButton
+          v-tooltip="t('main_navigation.delete_workout')"
+          variant="ghost"
+          icon-only
+          @click.stop="emit('deleteWorkout')"
+        >
+          <TheIcon
+            icon-name="trash-can"
+            width="18px"
+          />
+        </TheButton>
+      </li>
+    </ul>
+
     <div
       v-if="showStartButton"
       class="start-workout__wr"
@@ -83,16 +126,43 @@ function showResultModal() {
       <div class="second-ring" />
     </div>
 
-    <RunWorkoutModal
-      ref="runWorkoutConfirm"
-      @start-workout="handleStartWorkout"
-    />
-
+    <!--    todo here -->
     <TheModal
       ref="readWorkoutResults"
     >
       <template #content>
         fsdfsdfs
+      </template>
+    </TheModal>
+
+    <TheModal
+      ref="runWorkoutConfirm"
+      :is-outside-close="false"
+      :has-close-button="false"
+      class="date-menu__start-workout"
+    >
+      <template #content>
+        <div class="date-menu__start-content">
+          <div class="date-menu__start-text">
+            <p>Вы приступите к выполнению тренировки.</p>
+            <p>Это следует делать, <b>если вы готовы начать прямо сейчас</b></p>
+          </div>
+        </div>
+      </template>
+      <template #footer>
+        <div class="date-menu__start-footer">
+          <TheButton
+            variant="ghost"
+            @click="closeRunWorkoutConfirm"
+          >
+            отменить
+          </TheButton>
+          <TheButton
+            @click="handleStartWorkout"
+          >
+            начать
+          </TheButton>
+        </div>
       </template>
     </TheModal>
   </div>
