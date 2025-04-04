5076791f0ff2d22ecb742015240ca65298a13466
konstantinbaskov <bashkovdev@icloud.com>
2025-04-04 09:03:16 +0400

    fix(others): optimize api constants

--- constants/api.ts
+++ constants/api.ts
@@ -6,22 +6,21 @@ export const API_CREATE_SET = '/api/workout/createSet'
 export const API_END = '/api/finish-workout/finishWorkout'
 export const API_COPY = '/api/workout/workoutCopy'
 
-export const API_CREATE = '/api/workout/create'
-export const API_UPDATE = '/api/workout/update'
-
 const GET_USER_EXERCISES = '/api/exercises/getUserExercises'
 const CREATE_USER_EXERCISE = '/api/exercises/createUserExercise'
 const DELETE_USER_EXERCISE = '/api/exercises/deleteUserExercise'
 const EXERCISES_LIST = '/api/exercises/exercises'
+
 const FINISH_WORKOUT = '/api/finish-workout/finishWorkout'
-const WORKOUTS_LIST = '/api/workout/workoutsListByUserId'
 const START_WORKOUT = '/api/start-workout/startWorkout'
-const RESET_WORKOUT = '/api/workout/resetWorkout'
 const GET_CHARTS_DATA = '/api/statistics/getChartsData'
-const DELETE_WORKOUT = '/api/workout/workoutDelete'
 const GLOBAL_STATISTICS = '/api/statistics/getGlobalStatistics'
+
+const RESET_WORKOUT = '/api/workout/resetWorkout'
+const DELETE_WORKOUT = '/api/workout/workoutDelete'
 const CREATE_WORKOUT = '/api/workout/create'
 const UPDATE_WORKOUT = '/api/workout/update'
+const WORKOUTS_LIST = '/api/workout/workoutsListByUserId'
 
 export const API: Record<string, string> = {
   CREATE_USER_EXERCISE,
