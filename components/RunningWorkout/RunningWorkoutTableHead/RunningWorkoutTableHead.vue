19c04ce3939f01f5f4abef3d7b7326f9bac4de8a
konstantinbaskov <bashkovdev@icloud.com>
2025-05-17 11:29:36 +0400

    refactor(RunningWorkout): remove icons and update padding styles in table components

--- components/RunningWorkout/RunningWorkoutTableHead/RunningWorkoutTableHead.vue
+++ components/RunningWorkout/RunningWorkoutTableHead/RunningWorkoutTableHead.vue
@@ -2,31 +2,15 @@
   <ul>
     <li class="set-header">
       <div class="set-header__cell">
-        <TheIcon
-          icon-name="chart-simple"
-          width="12"
-        />
         Level
       </div>
       <div class="set-header__cell">
-        <TheIcon
-          icon-name="weight-hanging"
-          width="12"
-        />
         Weight
       </div>
       <div class="set-header__cell">
-        <TheIcon
-          icon-name="repeat"
-          width="12"
-        />
         Repeats
       </div>
       <div class="set-header__cell">
-        <TheIcon
-          icon-name="clock"
-          width="12"
-        />
         Time
       </div>
       <div class="set-header__cell" />
