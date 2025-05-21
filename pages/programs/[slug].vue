bd8944fc1974425e232c51eb4da35547e5d002d6
konstantinbaskov <bashkovdev@icloud.com>
2025-05-21 12:51:46 +0400

    error boundry programs list

--- pages/programs/[slug].vue
+++ pages/programs/[slug].vue
@@ -1,7 +1,18 @@
 <script setup lang="ts">
 const route = useRoute()
+
+const allowedSlugs = ['linear', 'undulating', 'block']
+
+onMounted(() => {
+  if (!allowedSlugs.includes(route.params.slug as string)) {
+    showError({
+      statusCode: 404,
+      message: 'Страница не найдена',
+    })
+  }
+})
 </script>
 
 <template>
-  <p>this is {{ route }}</p>
+  <p>this is {{ route.params.slug }}</p>
 </template>
