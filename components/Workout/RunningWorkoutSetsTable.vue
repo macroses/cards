08f8890166d90e398c75f8eee94d93e81db44f05
konstantinbaskov <bashkovdev@icloud.com>
2025-05-30 09:37:15 +0400

    refactor(workout): modularize-running-sets-table - extract running workout sets table to a dedicated component

---
+++ components/Workout/RunningWorkoutSetsTable.vue
@@ -0,0 +1,220 @@
+<script setup lang="ts">
+import type { TrainingSession } from '~/ts'
+import type { UnionSetFields } from '~/ts/types/setFields.types'
+import dayjs from 'dayjs'
+import { vMaska } from 'maska/vue'
+// import { validationSchemas } from '@/validation/schemas/common.schema'
+import { WORKOUT_DIFFICULTY } from '~/constants'
+
+defineProps<{
+  exercise: {
+    sets: TrainingSession[]
+    name: string
+  }
+  exerciseId: string
+  activeExerciseId: string | null
+}>()
+
+const emit = defineEmits<{
+  deleteSet: [setId: string]
+  updateField: [
+    setId: string,
+    field: UnionSetFields,
+    value: number | string,
+  ]
+  updateSetTime: [setId: string]
+}>()
+
+// const { t } = useI18n()
+// const { createExerciseSetSchema } = validationSchemas(t)
+//
+// const { defineField, errors, values, meta } = useForm({
+//   validationSchema: toTypedSchema(createExerciseSetSchema),
+// })
+//
+// const [weight] = defineField('weight')
+// const [repeats] = defineField('repeats')
+
+const editingSetId = shallowRef<string | null>(null)
+const editingValue = shallowRef<number | string>(0)
+const editingField = shallowRef<UnionSetFields | null>(null)
+
+function startEditing(set: TrainingSession, field: UnionSetFields) {
+  editingSetId.value = set.id
+  editingField.value = field
+
+  if (field !== 'setTime') {
+    editingValue.value = set[field] ?? 0
+    return
+  }
+
+  editingValue.value = dayjs.duration(set[field] || 0, 'seconds').format('mm:ss')
+}
+
+async function saveEdit() {
+  if (!editingSetId.value || !editingField.value) {
+    return
+  }
+
+  if (editingField.value === 'setTime') {
+    editingValue.value = formattedTime(editingValue.value as string)
+  }
+
+  emit(
+    'updateField',
+    editingSetId.value,
+    editingField.value,
+    editingValue.value,
+  )
+
+  editingSetId.value = null
+  editingField.value = null
+}
+
+function handleKeyDown(event: KeyboardEvent) {
+  if (event.key !== 'Enter') {
+    return
+  }
+
+  saveEdit()
+}
+
+function isInputVisible(setId: string, field: UnionSetFields) {
+  return editingSetId.value === setId && editingField.value === field
+}
+
+function handleDifficultyChange(setId: string, value: number | undefined) {
+  if (!value) {
+    return
+  }
+
+  emit('updateField', setId, 'difficulty', value)
+}
+</script>
+
+<template>
+  <div
+    v-if="activeExerciseId === exerciseId"
+    class="sets-table"
+    :class="{ active: activeExerciseId === exerciseId }"
+  >
+    <div
+      v-auto-animate="{ duration: 100 }"
+      class="sets-table__content"
+    >
+      <RunningWorkoutTableHead />
+      <div
+        v-for="set in exercise.sets"
+        :key="set.id"
+        class="set-row"
+      >
+        <div class="set-cell">
+          <TheDropdown
+            v-model="set.difficulty"
+            no-icon
+            @update:model-value="handleDifficultyChange(set.id, $event)"
+          >
+            <template #default="{ selectValue }">
+              <li
+                v-for="difficulty in WORKOUT_DIFFICULTY"
+                :key="difficulty.value"
+                class="difficulty__list-item"
+                @click="selectValue(difficulty.value)"
+              >
+                <span :class="`difficulty-${difficulty.value}`" />
+                {{ difficulty.value }}
+              </li>
+            </template>
+          </TheDropdown>
+        </div>
+        <div class="set-cell">
+          <TheInput
+            v-if="isInputVisible(set.id, 'weight')"
+            v-model="editingValue"
+            v-focus
+            type="number"
+            :placeholder="$t('inputs.weight')"
+            inputmode="decimal"
+            class="edit-input"
+            @blur="saveEdit"
+            @keydown.stop="handleKeyDown"
+          />
+          <div
+            v-else
+            class="editable-value"
+            @click="startEditing(set, 'weight')"
+          >
+            {{ set.weight }}
+          </div>
+        </div>
+
+        <div class="set-cell">
+          <TheInput
+            v-if="isInputVisible(set.id, 'repeats')"
+            v-model="editingValue"
+            v-focus
+            type="number"
+            :placeholder="$t('inputs.repeats')"
+            inputmode="numeric"
+            class="edit-input"
+            @blur="saveEdit"
+            @keydown.stop="handleKeyDown"
+          />
+          <div
+            v-else
+            class="editable-value"
+            @click="startEditing(set, 'repeats')"
+          >
+            {{ set.repeats }}
+          </div>
+        </div>
+        <div class="set-cell">
+          <TheInput
+            v-if="isInputVisible(set.id, 'setTime') && set.setTimeAddedAt"
+            v-model="editingValue"
+            v-focus
+            v-maska="'##:##'"
+            type="text"
+            :placeholder="$t('inputs.time_format')"
+            inputmode="numeric"
+            class="edit-input"
+            @blur="saveEdit"
+            @keydown.stop="handleKeyDown"
+          />
+          <div
+            v-else-if="set.setTimeAddedAt"
+            class="editable-value"
+            @click="startEditing(set, 'setTime')"
+          >
+            {{ dayjs.duration(set.setTime || 0, 'seconds').format('mm:ss') }}
+          </div>
+          <TheButton
+            v-if="!set.setTimeAddedAt"
+            variant="secondary"
+            class="mark-set-time"
+            @click="emit('updateSetTime', set.id)"
+          >
+            <TheIcon
+              icon-name="clock"
+              width="18"
+            />
+          </TheButton>
+        </div>
+        <div class="set-cell">
+          <TheButton
+            variant="transparent"
+            icon-only
+            class="delete-set__button"
+            :class="{ disabled: exercise.sets.length === 1 }"
+            @click="emit('deleteSet', set.id)"
+          >
+            <TheIcon
+              icon-name="trash-can"
+              width="18"
+            />
+          </TheButton>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
--- pages/workout/run/[id].vue
+++ pages/workout/run/[id].vue
@@ -3,8 +3,6 @@ import type TheModal from '~/components/ui/TheModal/TheModal.vue'
 import type { TrainingSession } from '~/ts'
 import type { UnionSetFields } from '~/ts/types/setFields.types'
 import dayjs from 'dayjs'
-import { vMaska } from 'maska/vue'
-import { WORKOUT_DIFFICULTY } from '~/constants'
 
 const { t } = useI18n()
 
@@ -31,16 +29,7 @@ watch(() => workout.value, (newWorkout) => {
   }
 }, { immediate: true })
 
-// Editing set states
-const editingSetId = shallowRef<string | null>(null)
-const editingValue = shallowRef<number | string>(0)
-const editingField = shallowRef<UnionSetFields | null>(null)
-
 function toggleExercise(exerciseId: string) {
-  if (editingSetId.value || editingField.value) {
-    return
-  }
-
   activeExerciseId.value = activeExerciseId.value === exerciseId ? null : exerciseId
 }
 
@@ -58,59 +47,6 @@ async function updateSetFieldValue(setId: string, field: UnionSetFields, value: 
     field,
     value,
   )
-}
-
-function startEditing(set: TrainingSession, field: UnionSetFields) {
-  editingSetId.value = set.id
-  editingField.value = field
-
-  if (field !== 'setTime') {
-    editingValue.value = set[field] ?? 0
-    return
-  }
-
-  editingValue.value = dayjs.duration(set[field] || 0, 'seconds').format('mm:ss')
-}
-
-async function saveEdit() {
-  if (!editingSetId.value || !editingField.value) {
-    return
-  }
-
-  let valueToSave = editingValue.value
-
-  if (editingField.value === 'setTime') {
-    valueToSave = formattedTime(editingValue.value as string)
-  }
-
-  await updateSetFieldValue(
-    editingSetId.value,
-    editingField.value,
-    valueToSave,
-  )
-
-  editingSetId.value = null
-  editingField.value = null
-}
-
-function handleKeyDown(event: KeyboardEvent) {
-  if (event.key !== 'Enter') {
-    return
-  }
-
-  saveEdit()
-}
-
-function isInputVisible(setId: string, field: UnionSetFields) {
-  return editingSetId.value === setId && editingField.value === field
-}
-
-function handleDifficultyChange(setId: string, value: number | undefined) {
-  if (!value) {
-    return
-  }
-
-  updateSetFieldValue(setId, 'difficulty', value)
 }
 
 // Editing set time
@@ -132,7 +68,11 @@ async function handleDeleteSet(setId: string) {
 }
 
 async function handleAddSet(exerciseId: string) {
-  await addSet(workout.value!, exerciseId)
+  if (!workout.value) {
+    return
+  }
+
+  await addSet(workout.value, exerciseId)
 }
 
 // Check if all sets in an exercise have time marked
@@ -226,130 +166,14 @@ useHead({
                 </TheButton>
               </div>
 
-              <div
-                v-if="activeExerciseId === exerciseId"
-                class="sets-table"
-                :class="{ active: activeExerciseId === exerciseId }"
-              >
-                <div
-                  v-auto-animate="{ duration: 100 }"
-                  class="sets-table__content"
-                >
-                  <RunningWorkoutTableHead />
-                  <div
-                    v-for="set in exercise.sets"
-                    :key="set.id"
-                    class="set-row"
-                  >
-                    <div class="set-cell">
-                      <TheDropdown
-                        v-model="set.difficulty"
-                        no-icon
-                        @update:model-value="handleDifficultyChange(set.id, $event)"
-                      >
-                        <template #default="{ selectValue }">
-                          <li
-                            v-for="difficulty in WORKOUT_DIFFICULTY"
-                            :key="difficulty.value"
-                            class="difficulty__list-item"
-                            @click="selectValue(difficulty.value)"
-                          >
-                            <span :class="`difficulty-${difficulty.value}`" />
-                            {{ difficulty.value }}
-                          </li>
-                        </template>
-                      </TheDropdown>
-                    </div>
-                    <div class="set-cell">
-                      <TheInput
-                        v-if="isInputVisible(set.id, 'weight')"
-                        v-model="editingValue"
-                        v-focus
-                        type="number"
-                        :placeholder="$t('inputs.weight')"
-                        inputmode="decimal"
-                        class="edit-input"
-                        @blur="saveEdit"
-                        @keydown.stop="handleKeyDown"
-                      />
-                      <div
-                        v-else
-                        class="editable-value"
-                        @click="startEditing(set, 'weight')"
-                      >
-                        {{ set.weight }}
-                      </div>
-                    </div>
-
-                    <div class="set-cell">
-                      <TheInput
-                        v-if="isInputVisible(set.id, 'repeats')"
-                        v-model="editingValue"
-                        v-focus
-                        type="number"
-                        :placeholder="$t('inputs.repeats')"
-                        inputmode="numeric"
-                        class="edit-input"
-                        @blur="saveEdit"
-                        @keydown.stop="handleKeyDown"
-                      />
-                      <div
-                        v-else
-                        class="editable-value"
-                        @click="startEditing(set, 'repeats')"
-                      >
-                        {{ set.repeats }}
-                      </div>
-                    </div>
-                    <div class="set-cell">
-                      <TheInput
-                        v-if="isInputVisible(set.id, 'setTime') && set.setTimeAddedAt"
-                        v-model="editingValue"
-                        v-focus
-                        v-maska="'##:##'"
-                        type="text"
-                        :placeholder="$t('inputs.time_format')"
-                        inputmode="numeric"
-                        class="edit-input"
-                        @blur="saveEdit"
-                        @keydown.stop="handleKeyDown"
-                      />
-                      <div
-                        v-else-if="set.setTimeAddedAt"
-                        class="editable-value"
-                        @click="startEditing(set, 'setTime')"
-                      >
-                        {{ dayjs.duration(set.setTime || 0, 'seconds').format('mm:ss') }}
-                      </div>
-                      <TheButton
-                        v-if="!set.setTimeAddedAt"
-                        variant="secondary"
-                        class="mark-set-time"
-                        @click="handleSetTimeUpdate(set.id)"
-                      >
-                        <TheIcon
-                          icon-name="clock"
-                          width="18"
-                        />
-                      </TheButton>
-                    </div>
-                    <div class="set-cell">
-                      <TheButton
-                        variant="transparent"
-                        icon-only
-                        class="delete-set__button"
-                        :class="{ disabled: exercise.sets.length === 1 }"
-                        @click="handleDeleteSet(set.id)"
-                      >
-                        <TheIcon
-                          icon-name="trash-can"
-                          width="18"
-                        />
-                      </TheButton>
-                    </div>
-                  </div>
-                </div>
-              </div>
+              <RunningWorkoutSetsTable
+                :exercise="exercise"
+                :exercise-id="exerciseId"
+                :active-exercise-id="activeExerciseId"
+                @update-field="updateSetFieldValue"
+                @update-set-time="handleSetTimeUpdate"
+                @delete-set="handleDeleteSet"
+              />
             </li>
           </ul>
 
